d43152bfe35df4e0e71940cbdd5ebb1c9893dc47
Simon Vana <simon.vana@gmail.com>
2013-02-18 15:24:14 -0800

    Switched from vbo to pyglet's vertex_list

--- Render.py
+++ Render.py
@@ -10,6 +10,21 @@ class Knot_Display(pyglet.window.Window):
     def __init__(self, **kwargs):
         super(Knot_Display, self).__init__(**kwargs)
 
+        self.gridwidth = 100
+        self.gridheight = 100
+
+        def _create_grid(width, height):
+            columns = [ float(x)/width for x in range(width) ]
+            rows = [ float(y)/height for y in range(height) ]
+            for r in rows:
+                for c in columns:
+                    yield r
+                    yield c
+
+        self.vertex_list = pyglet.graphics.vertex_list(self.gridwidth * self.gridheight, 'v2f')
+        self.vertex_list.vertices = list(_create_grid(self.gridwidth, self.gridheight))
+
+
         self.t = 0.0
         self.orientation = Quat(1, Vec3(0,0,0))
 
@@ -26,14 +41,6 @@ class Knot_Display(pyglet.window.Window):
                                 gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * p;
                             }''')
         )
-
-    def init_grid(self, points):
-        point_data = [ coord for point in points for coord in point ]
-        num_coords = len(point_data)
-        pointsGl = (GLfloat * num_coords)(*point_data)
-        self.vbo = pyglet.graphics.vertexbuffer.create_buffer(
-            num_coords*4, GL_ARRAY_BUFFER, GL_STATIC_DRAW)
-        self.vbo.set_data(pointsGl)
 
     def update(self, dt):
         self.t += dt
@@ -57,7 +64,6 @@ class Knot_Display(pyglet.window.Window):
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glTranslatef(0, 0, -7.0)
-        #glRotatef(self.t*4, 1.0, 1.0, 0.0)
 
         r = self.orientation.conj().matrix()
         # column-major order
@@ -70,35 +76,19 @@ class Knot_Display(pyglet.window.Window):
             array[index] = value
         glMultMatrixf(array);
 
-        # glTranslatef(-0.5, -0.5, 0)
         glPointSize(1.8)
         with self.program:
-            self.vbo.bind()
-            glEnableVertexAttribArray(0)
-            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0)
-            glDrawArrays(GL_POINTS, 0, 10000)
-            glDisableVertexAttribArray(0)
-            self.vbo.unbind()
+            self.vertex_list.draw(GL_POINTS)
         glPopMatrix()
 
 
 def main():
 
-    def _create_grid(width, height):
-        grid = []
-        columns = [ float(x)/width for x in range(width) ]
-        rows = [ float(y)/height for y in range(height) ]
-        for r in rows:
-            for c in columns:
-                grid.append((r,c))
-        return grid
 
 
     config = pyglet.gl.Config(sample_buffers=1, samples=4)
     window = Knot_Display(caption='Knotviz in the house', resizable=True, vsync=True, config=config)
 
-    points = _create_grid(100, 100)
-    window.init_grid(points)
 
     pyglet.clock.schedule_interval(window.update, (1.0/60))
     pyglet.app.run()
